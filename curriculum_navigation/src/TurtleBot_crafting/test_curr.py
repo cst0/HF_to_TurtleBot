25410300db04b60d55b58126d9bb43530622ff0e
Chris Thierauf <christopher.thierauf@tufts.edu>
2021-06-16 21:20:08 -0400

    getting stuff running

--- curriculum_navigation/src/TurtleBot_crafting/test_curr.py
+++ curriculum_navigation/src/TurtleBot_crafting/test_curr.py
@@ -1,9 +1,12 @@
+#!/usr/bin/env python3.7
+
 import os
 import sys
 
 import gym
 import time
 import numpy as np
+
 # import gym_novel_gridworlds
 import TurtleBot_v0
 
@@ -13,250 +16,287 @@ from SimpleDQN import SimpleDQN
 import matplotlib.pyplot as plt
 
 
-
 def CheckTrainingDoneCallback(reward_array, done_array, env):
 
-	done_cond = False
-	reward_cond = False
-	if len(done_array) > 40:
-		if np.mean(done_array[-10:]) > 0.85 and np.mean(done_array[-40:]) > 0.85:
-			if abs(np.mean(done_array[-40:]) - np.mean(done_array[-10:])) < 0.5:
-				done_cond = True
+    done_cond = False
+    reward_cond = False
+    if len(done_array) > 40:
+        if np.mean(done_array[-10:]) > 0.85 and np.mean(done_array[-40:]) > 0.85:
+            if abs(np.mean(done_array[-40:]) - np.mean(done_array[-10:])) < 0.5:
+                done_cond = True
 
-		if done_cond == True:
-			if env < 3:
-				if np.mean(reward_array[-40:]) > 730:
-					reward_cond = True
-			# else:
-			# 	if np.mean(reward_array[-10:]) > 950:
-			# 		reward_cond = True
+        if done_cond == True:
+            if env < 3:
+                if np.mean(reward_array[-40:]) > 730:
+                    reward_cond = True
+            # else:
+            # 	if np.mean(reward_array[-10:]) > 950:
+            # 		reward_cond = True
 
-		if done_cond == True and reward_cond == True:
-			return 1
-		else:
-			return 0
-	else:
-		return 0
-
+        if done_cond == True and reward_cond == True:
+            return 1
+        else:
+            return 0
+    else:
+        return 0
 
 
 if __name__ == "__main__":
 
-	no_of_environmets = 4
+    no_of_environmets = 4
 
-	width_array = [1.5,2.5,3,3]
-	height_array = [1.5,2.5,3,3]
-	no_trees_array = [1,1,3,4]
-	no_rocks_array = [0,1,2,2]
-	crafting_table_array = [0,0,1,1]
-	starting_trees_array = [0,0,0,0]
-	starting_rocks_array = [0,0,0,0]
-	type_of_env_array = [0,1,2,2]
+    width_array = [1.5, 2.5, 3, 3]
+    height_array = [1.5, 2.5, 3, 3]
+    no_trees_array = [1, 1, 3, 4]
+    no_rocks_array = [0, 1, 2, 2]
+    crafting_table_array = [0, 0, 1, 1]
+    starting_trees_array = [0, 0, 0, 0]
+    starting_rocks_array = [0, 0, 0, 0]
+    type_of_env_array = [0, 1, 2, 2]
 
-	total_timesteps_array = []
-	total_reward_array = []
-	avg_reward_array = []
-	final_timesteps_array = []
-	final_reward_array = []
-	final_avg_reward_array = []
-	task_completion_array = []
+    total_timesteps_array = []
+    total_reward_array = []
+    avg_reward_array = []
+    final_timesteps_array = []
+    final_reward_array = []
+    final_avg_reward_array = []
+    task_completion_array = []
 
+    actionCnt = 5
+    D = 83  # 90 beams x 4 items lidar + 3 inventory items
+    NUM_HIDDEN = 16
+    GAMMA = 0.995
+    LEARNING_RATE = 1e-3
+    DECAY_RATE = 0.99
+    MAX_EPSILON = 0.1
+    random_seed = 1
 
+    # agent = SimpleDQN(actionCnt,D,NUM_HIDDEN,LEARNING_RATE,GAMMA,DECAY_RATE,MAX_EPSILON,random_seed)
+    # agent.set_explore_epsilon(MAX_EPSILON)
+    action_space = ["W", "A", "D", "U", "C"]
+    total_episodes_arr = []
 
-	actionCnt = 5
-	D = 83 #90 beams x 4 items lidar + 3 inventory items
-	NUM_HIDDEN = 16
-	GAMMA = 0.995
-	LEARNING_RATE = 1e-3
-	DECAY_RATE = 0.99
-	MAX_EPSILON = 0.1
-	random_seed = 1
+    for k in range(1):
+        i = 3
+        print("Environment: ", i)
 
-	# agent = SimpleDQN(actionCnt,D,NUM_HIDDEN,LEARNING_RATE,GAMMA,DECAY_RATE,MAX_EPSILON,random_seed)
-	# agent.set_explore_epsilon(MAX_EPSILON)
-	action_space = ['W','A','D','U','C']
-	total_episodes_arr = []
+        width = width_array[i]
+        height = height_array[i]
+        no_trees = no_trees_array[i]
+        no_rocks = no_rocks_array[i]
+        crafting_table = crafting_table_array[i]
+        starting_trees = starting_trees_array[i]
+        starting_rocks = starting_rocks_array[i]
+        type_of_env = type_of_env_array[i]
 
-	for k in range(1):
-		i = 3
-		print("Environment: ", i)
+        final_status = False
 
-		width = width_array[i]
-		height = height_array[i]
-		no_trees = no_trees_array[i]
-		no_rocks = no_rocks_array[i]
-		crafting_table = crafting_table_array[i]
-		starting_trees = starting_trees_array[i]
-		starting_rocks = starting_rocks_array[i]
-		type_of_env = type_of_env_array[i]
+        if i == 0:
+            agent = SimpleDQN(
+                actionCnt,
+                D,
+                NUM_HIDDEN,
+                LEARNING_RATE,
+                GAMMA,
+                DECAY_RATE,
+                MAX_EPSILON,
+                random_seed,
+            )
+            agent.set_explore_epsilon(MAX_EPSILON)
+        else:
+            agent = SimpleDQN(
+                actionCnt,
+                D,
+                NUM_HIDDEN,
+                LEARNING_RATE,
+                GAMMA,
+                DECAY_RATE,
+                MAX_EPSILON,
+                random_seed,
+            )
+            agent.set_explore_epsilon(MAX_EPSILON)
+            agent.load_model(0, 0, i)
+            agent.reset()
+            print("loaded model")
 
-		final_status = False
+        if i == no_of_environmets - 1:
+            final_status = True
 
-		if i == 0:
-			agent = SimpleDQN(actionCnt,D,NUM_HIDDEN,LEARNING_RATE,GAMMA,DECAY_RATE,MAX_EPSILON,random_seed)
-			agent.set_explore_epsilon(MAX_EPSILON)
-		else:
-			agent = SimpleDQN(actionCnt,D,NUM_HIDDEN,LEARNING_RATE,GAMMA,DECAY_RATE,MAX_EPSILON,random_seed)
-			agent.set_explore_epsilon(MAX_EPSILON)
-			agent.load_model(0,0,i)
-			agent.reset()
-			print("loaded model")
+        env_id = "TurtleBot-v0"
+        env = gym.make(
+            env_id,
+            map_width=width,
+            map_height=height,
+            items_quantity={
+                "tree": no_trees,
+                "rock": no_rocks,
+                "crafting_table": crafting_table,
+                "pogo_stick": 0,
+            },
+            initial_inventory={
+                "wall": 0,
+                "tree": starting_trees,
+                "rock": starting_rocks,
+                "crafting_table": 0,
+                "pogo_stick": 0,
+            },
+            goal_env=type_of_env,
+            is_final=final_status,
+        )
+        print("seed: ", env.seed())
+        # time.sleep(4)
+        t_step = 0
+        episode = 0
+        t_limit = 600
+        reward_sum = 0
+        reward_arr = []
+        avg_reward = []
+        done_arr = []
+        env_flag = 0
 
+        env.reset()
+        # time.sleep(10)
+        while True:
 
-		if i == no_of_environmets-1:
-			final_status = True
+            # get obseration from sensor
+            obs = env.get_observation()
+            # time.sleep(0.1)
+            # act
+            a = agent.process_step(obs, False)
 
-		env_id = 'TurtleBot-v0'
-		env = gym.make(env_id, map_width = width, map_height = height, items_quantity = {'tree': no_trees, 'rock': no_rocks, 'crafting_table': crafting_table, 'pogo_stick':0},
-			initial_inventory = {'wall': 0, 'tree': starting_trees, 'rock': starting_rocks, 'crafting_table': 0, 'pogo_stick':0}, goal_env = type_of_env, is_final = final_status)
-		print("seed: "	, env.seed())
-		# time.sleep(4)
-		t_step = 0
-		episode = 0
-		t_limit = 600
-		reward_sum = 0
-		reward_arr = []
-		avg_reward = []
-		done_arr = []
-		env_flag = 0
+            new_obs, reward, done, info = env.step(a)
+            print("At time: {}, action taken: {}".format(t_step, a))
+            time.sleep(0.1)
 
-		env.reset()
-		# time.sleep(10)
-		while True:
-			
-			# get obseration from sensor
-			obs = env.get_observation()
-			# time.sleep(0.1)
-			# act 
-			a = agent.process_step(obs,False)
-			
-			new_obs, reward, done, info = env.step(a)
-			print("At time: {}, action taken: {}".format(t_step,a))
-			time.sleep(0.1)
+            # give reward
+            agent.give_reward(reward)
+            reward_sum += reward
 
-			# give reward
-			agent.give_reward(reward)
-			reward_sum += reward
-			
-			t_step += 1
-			
-			if t_step > t_limit or done == True:
-				
-				print("Timesteps taken: ", t_step)
-				# finish agent
-				if done == True:
-					done_arr.append(1)
-					task_completion_array.append(1)
-				elif t_step > t_limit:
-					done_arr.append(0)
-					task_completion_array.append(0)
-				
-				print("\n\nfinished episode = "+str(episode)+" with " +str(reward_sum)+"\n")
-				time.sleep(5)
-				reward_arr.append(reward_sum)
-				avg_reward.append(np.mean(reward_arr[-40:]))
+            t_step += 1
 
-				total_reward_array.append(reward_sum)
-				avg_reward_array.append(np.mean(reward_arr[-40:]))
-				total_timesteps_array.append(t_step)
-		
-				done = True
-				t_step = 0
-				agent.finish_episode()
-			
-				# update after every episode
-				# if episode % 10 == 0:
-				# 	agent.update_parameters()
-			
-				# reset environment
-				episode += 1
+            if t_step > t_limit or done == True:
 
-				env.reset()
-				reward_sum = 0
+                print("Timesteps taken: ", t_step)
+                # finish agent
+                if done == True:
+                    done_arr.append(1)
+                    task_completion_array.append(1)
+                elif t_step > t_limit:
+                    done_arr.append(0)
+                    task_completion_array.append(0)
 
-				env_flag = 0
-				if i< 3:
-					env_flag = CheckTrainingDoneCallback(reward_arr, done_arr, i)
-		
-				# quit after some number of episodes
-				if episode > 0 or env_flag == 1:
+                print(
+                    "\n\nfinished episode = "
+                    + str(episode)
+                    + " with "
+                    + str(reward_sum)
+                    + "\n"
+                )
+                time.sleep(5)
+                reward_arr.append(reward_sum)
+                avg_reward.append(np.mean(reward_arr[-40:]))
 
-					# agent.save_model(0,0,i)
-					# total_episodes_arr.append(episode)
+                total_reward_array.append(reward_sum)
+                avg_reward_array.append(np.mean(reward_arr[-40:]))
+                total_timesteps_array.append(t_step)
 
-					break
+                done = True
+                t_step = 0
+                agent.finish_episode()
 
-	# print("Total epsiode array is: ", total_episodes_arr)
+                # update after every episode
+                # if episode % 10 == 0:
+                # 	agent.update_parameters()
 
-	# log_dir = 'logs_' + str(random_seed)
-	# os.makedirs(log_dir, exist_ok = True)
+                # reset environment
+                episode += 1
 
-	# total_timesteps_array = np.asarray(total_timesteps_array)
-	# print("size total_timesteps_array: ", total_timesteps_array.shape)
-	
-	# total_reward_array = np.asarray(total_reward_array)
-	# print("size total_reward_array: ", total_reward_array.shape)
+                env.reset()
+                reward_sum = 0
 
-	# avg_reward_array = np.asarray(avg_reward_array)
-	# print("size avg_reward_array: ", avg_reward_array.shape)
+                env_flag = 0
+                if i < 3:
+                    env_flag = CheckTrainingDoneCallback(reward_arr, done_arr, i)
 
-	# total_episodes_arr = np.asarray(total_episodes_arr)
-	# print("size total_episodes_arr: ", total_episodes_arr.shape)
+                # quit after some number of episodes
+                if episode > 0 or env_flag == 1:
 
-	# task_completion_arr = np.asarray(task_completion_array)
+                    # agent.save_model(0,0,i)
+                    # total_episodes_arr.append(episode)
 
-	# # final_timesteps_array = np.asarray(final_timesteps_array)
-	# # print("size final_timesteps_array: ", final_timesteps_array.shape)
+                    break
 
-	# # final_reward_array = np.asarray(final_reward_array)
-	# # print("size final_reward_array: ", final_reward_array.shape)
+    # print("Total epsiode array is: ", total_episodes_arr)
 
-	# # final_avg_reward_array = np.asarray(final_avg_reward_array)
-	# # print("size final_avg_reward_array: ", final_avg_reward_array.shape)
+    # log_dir = 'logs_' + str(random_seed)
+    # os.makedirs(log_dir, exist_ok = True)
 
-	# experiment_file_name_total_timesteps = 'randomseed_' + str(random_seed) + '_total_timesteps'
-	# path_to_save_total_timesteps = log_dir + os.sep + experiment_file_name_total_timesteps + '.npz'
+    # total_timesteps_array = np.asarray(total_timesteps_array)
+    # print("size total_timesteps_array: ", total_timesteps_array.shape)
 
-	# experiment_file_name_total_reward = 'randomseed_' + str(random_seed) + '_total_reward'
-	# path_to_save_total_reward = log_dir + os.sep + experiment_file_name_total_reward + '.npz'
+    # total_reward_array = np.asarray(total_reward_array)
+    # print("size total_reward_array: ", total_reward_array.shape)
 
-	# experiment_file_name_avg_reward = 'randomseed_' + str(random_seed) + '_avg_reward'
-	# path_to_save_avg_reward = log_dir + os.sep + experiment_file_name_avg_reward + '.npz'
+    # avg_reward_array = np.asarray(avg_reward_array)
+    # print("size avg_reward_array: ", avg_reward_array.shape)
 
-	# experiment_file_name_total_episodes = 'randomseed_' + str(random_seed) + '_total_episodes'
-	# path_to_save_total_episodes = log_dir + os.sep + experiment_file_name_total_episodes + '.npz'
+    # total_episodes_arr = np.asarray(total_episodes_arr)
+    # print("size total_episodes_arr: ", total_episodes_arr.shape)
 
-	# experiment_file_name_task_completion = 'randomseed_' + str(random_seed) + '_task_completion_curr'
-	# path_to_save_task_completion = log_dir + os.sep + experiment_file_name_task_completion + '.npz'
+    # task_completion_arr = np.asarray(task_completion_array)
 
-	# # experiment_file_name_final_timesteps = 'randomseed_' + str(random_seed) + '_final_timesteps'
-	# # path_to_save_final_timesteps = log_dir + os.sep + experiment_file_name_final_timesteps + '.npz'
+    # # final_timesteps_array = np.asarray(final_timesteps_array)
+    # # print("size final_timesteps_array: ", final_timesteps_array.shape)
 
-	# # experiment_file_name_final_reward = 'randomseed_' + str(random_seed) + '_final_reward'
-	# # path_to_save_final_reward = log_dir + os.sep + experiment_file_name_final_reward + '.npz'
+    # # final_reward_array = np.asarray(final_reward_array)
+    # # print("size final_reward_array: ", final_reward_array.shape)
 
-	# # experiment_file_name_final_avg_reward = 'randomseed_' + str(random_seed) + '_final_avg_reward'
-	# # path_to_save_final_avg_reward = log_dir + os.sep + experiment_file_name_final_avg_reward + '.npz'
+    # # final_avg_reward_array = np.asarray(final_avg_reward_array)
+    # # print("size final_avg_reward_array: ", final_avg_reward_array.shape)
 
-	# np.savez_compressed(path_to_save_total_timesteps, curriculum_timesteps = total_timesteps_array)
-	# # np.delete(total_timesteps_array)
+    # experiment_file_name_total_timesteps = 'randomseed_' + str(random_seed) + '_total_timesteps'
+    # path_to_save_total_timesteps = log_dir + os.sep + experiment_file_name_total_timesteps + '.npz'
 
-	# np.savez_compressed(path_to_save_total_reward, curriculum_reward = total_reward_array)
-	# # np.delete(total_reward_array)
+    # experiment_file_name_total_reward = 'randomseed_' + str(random_seed) + '_total_reward'
+    # path_to_save_total_reward = log_dir + os.sep + experiment_file_name_total_reward + '.npz'
 
-	# np.savez_compressed(path_to_save_avg_reward, curriculum_avg_reward = avg_reward_array)
-	# # np.delete(avg_reward_array)
+    # experiment_file_name_avg_reward = 'randomseed_' + str(random_seed) + '_avg_reward'
+    # path_to_save_avg_reward = log_dir + os.sep + experiment_file_name_avg_reward + '.npz'
 
-	# np.savez_compressed(path_to_save_total_episodes, curriculum_episodes = total_episodes_arr)
-	# # np.delete(total_episodes_arr)
+    # experiment_file_name_total_episodes = 'randomseed_' + str(random_seed) + '_total_episodes'
+    # path_to_save_total_episodes = log_dir + os.sep + experiment_file_name_total_episodes + '.npz'
 
-	# np.savez_compressed(path_to_save_task_completion, task_completion_curr = task_completion_arr)
+    # experiment_file_name_task_completion = 'randomseed_' + str(random_seed) + '_task_completion_curr'
+    # path_to_save_task_completion = log_dir + os.sep + experiment_file_name_task_completion + '.npz'
 
-	# # np.savez_compressed(path_to_save_final_timesteps, final_timesteps = final_timesteps_array)
-	# # # np.delete(final_timesteps_array)
+    # # experiment_file_name_final_timesteps = 'randomseed_' + str(random_seed) + '_final_timesteps'
+    # # path_to_save_final_timesteps = log_dir + os.sep + experiment_file_name_final_timesteps + '.npz'
 
-	# # np.savez_compressed(path_to_save_final_reward, final_reward = final_reward_array)
-	# # # final_reward_array.cler()
+    # # experiment_file_name_final_reward = 'randomseed_' + str(random_seed) + '_final_reward'
+    # # path_to_save_final_reward = log_dir + os.sep + experiment_file_name_final_reward + '.npz'
 
-	# # np.savez_compressed(path_to_save_final_avg_reward, final_avg_reward = final_avg_reward_array)
-	# # # np.delete(final_avg_reward_array)
+    # # experiment_file_name_final_avg_reward = 'randomseed_' + str(random_seed) + '_final_avg_reward'
+    # # path_to_save_final_avg_reward = log_dir + os.sep + experiment_file_name_final_avg_reward + '.npz'
+
+    # np.savez_compressed(path_to_save_total_timesteps, curriculum_timesteps = total_timesteps_array)
+    # # np.delete(total_timesteps_array)
+
+    # np.savez_compressed(path_to_save_total_reward, curriculum_reward = total_reward_array)
+    # # np.delete(total_reward_array)
+
+    # np.savez_compressed(path_to_save_avg_reward, curriculum_avg_reward = avg_reward_array)
+    # # np.delete(avg_reward_array)
+
+    # np.savez_compressed(path_to_save_total_episodes, curriculum_episodes = total_episodes_arr)
+    # # np.delete(total_episodes_arr)
+
+    # np.savez_compressed(path_to_save_task_completion, task_completion_curr = task_completion_arr)
+
+    # # np.savez_compressed(path_to_save_final_timesteps, final_timesteps = final_timesteps_array)
+    # # # np.delete(final_timesteps_array)
+
+    # # np.savez_compressed(path_to_save_final_reward, final_reward = final_reward_array)
+    # # # final_reward_array.cler()
+
+    # # np.savez_compressed(path_to_save_final_avg_reward, final_avg_reward = final_avg_reward_array)
+    # # # np.delete(final_avg_reward_array)
